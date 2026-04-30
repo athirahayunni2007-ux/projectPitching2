1a0227ce8f87237444bf73b2e8f21e6fb3243ad3
AJ <jazminaanis99@gmail.com>
2026-04-30 14:28:43 +0800

    feature/logInSignUp

---
+++ authentification.js
@@ -0,0 +1,60 @@
+console.log("File js sudah berjaya dimuatkan");
+
+function showPassword(){
+
+    let passwordInput =document.getElementById("password");
+
+    let checkbox = document.getElementById("showPassword")
+
+    if (checkbox.checked){
+        passwordInput.type = "text"
+    }
+    else{
+        passwordInput.type = "password";
+    }
+}
+function signUp() {
+
+    //take data from local storage
+    let userList = JSON.parse(localStorage.getItem("allUsers")) || [];
+
+
+    let newUsers = {
+        name: document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        password : document.getElementById("password").value
+    }
+
+    userList.push (newUsers);
+
+    localStorage.setItem("allUsers", JSON.stringify(userList));
+
+    console.log ("Updated list:",userList);
+    alert("Successfully sign up!");
+
+    window.location.href ="screening.html";
+}  
+
+function login() {
+    
+    let loginEmail = document.getElementById("email").value;
+    let loginPassword = document.getElementById("password").value;
+
+    let userList = JSON.parse(localStorage.getItem("allUsers")) || [];
+
+    let foundUser = userList.find(user => user.email === loginEmail);
+
+    if (!foundUser) {
+        alert("Account not found. Please sign up first!");
+        window.location.href = "signup.html"; // Terus hantar dia ke page sign up
+    } 
+
+    else if (foundUser.password === loginPassword) {
+        alert("Login successfully!");
+        window.location.href = "index.html";
+    } 
+
+    else {
+        alert("Wrong password. Please try again.");
+    }
+}
